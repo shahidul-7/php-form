e7bbcc9c406d5fa7c08a684280973e7293a7baa6
Shahidul <shahidul.du34@gmail.com>
2023-05-31 16:11:46 +0600

    PHP form added

---
+++ registration.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+    
+
+Welcome <?php echo $_POST["name"]; ?>!<br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your Phone Number is: <?php echo $_POST["phone"]; ?><br>
+Your message: <?php echo $_POST["message"]; ?>
+
+
+</body>
+</html>
